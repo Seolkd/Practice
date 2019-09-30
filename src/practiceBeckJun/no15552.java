0a4b539f2e1fe162ae0363fc01975cef610f3b32
seolkd <seold.e@gmail.com>
2019-10-01 00:02:46 +0900

    15552 add

--- src/practiceBeckJun/no15552.java
+++ src/practiceBeckJun/no15552.java
@@ -1,13 +1,33 @@
 package practiceBeckJun;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class no15552 {
-	public static void main(String[] args) {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String s bf.readLine();
-		int i Integer.parseInt(bf.readLine());
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int line = Integer.parseInt(br.readLine());
+		int[] a = new int[line];
+		int[] b = new int[line];
+		String[] str = new String[line];
+		StringTokenizer stok;
+		for (int i = 0; i < str.length; i++) {
+			str[i] = br.readLine();
+			stok = new StringTokenizer(str[i]);
+			a[i] = Integer.parseInt(stok.nextToken());
+			b[i] = Integer.parseInt(stok.nextToken());
+		}
+		for (int i = 0; i < str.length; i++) {
+			bw.write(a[i]+b[i]+"\n");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
+
 }
